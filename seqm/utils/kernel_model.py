3081ffac6b0fc7f4cfa44b8c893d3dcac6b1b4a2
Martin Stoehr <martin.stoehr.research@gmail.com>
2023-02-06 19:56:08 -0800

    Full seqm runners and AMASE modules (core and wrapped), next: abstract loss

--- seqm/utils/kernel_model.py
+++ seqm/utils/kernel_model.py
@@ -1,7 +1,22 @@
+############################################################################
+# Utilities for running SEQM calculations with kernel-predicted parameters #
+#  - ParameterKernel: get kernels and kernel-predicted parameters          #
+#  - AMASE_singlepoint: Regression coeffs, atomic numbers, positions/      #
+#                         descriptors defined at runtime                   #
+#  - AMASE_multirun: Regression coefficients at runtime, systems fixed     #
+#                                                                          #
+# Curent (Feb/06): Basic implementation                                    #
+# TODO:  . typing                                                          #
+#        . refactor                                                        #
+#        . ?add MD engine?                                                 #
+#        . custom backwards for (unlikely) RuntimeErrors in foward         #
+############################################################################
+
 import torch
 from itertools import chain
-from .pyseqm_helpers import prepare_array, get_default_parameters, get_parameter_names
-from .seqm_runners import SEQM_singlepoint, SEQM_multirun
+from .pyseqm_helpers import (prepare_array, Orderator,
+    get_default_parameters, get_parameter_names)
+from .seqm_runners import SEQM_singlepoint_core, SEQM_multirun_core
 
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -9,15 +24,15 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 class ParameterKernel(torch.nn.Module):
     def __init__(self, Z_ref, desc_ref):
         super(ParameterKernel, self).__init__()
-        c1 = isinstance(Z_ref, list) and torch.is_tensor(Z_ref[0])
-        c2 = isinstance(desc_ref, list) and torch.is_tensor(desc_ref[0])
-        if not (c1 and c2):
-            msg  = "Atomic numbers and descriptors have to be provided "
-            msg += "as list of tensors!"
+        if not torch.is_tensor(Z_ref):
+            msg  = "Atomic numbers have to be provided as tensors!"
             raise ValueError(msg)
-        Zflat = list(chain(*[z_i.tolist() for z_i in Z_ref]))
-        self.elements_ref = sorted(set(Zflat))
-        Zall = torch.tensor(Zflat)
+        if not isinstance(desc_ref, list) and torch.is_tensor(desc_ref[0]):
+            msg = "Descriptors have to be provided as list of tensors!"
+            raise ValueError(msg)
+        nondummy = (Z_ref > 0).reshape(-1)
+        Zall = Z_ref.reshape(-1)[nondummy]
+        self.elements_ref = sorted(set(Zall.tolist()))
         self.X_ref, self.idx_ref = {}, {}
         for elm in self.elements_ref:
             idx   = [torch.where(s==elm)[0] for s in Z_ref]
@@ -36,8 +51,8 @@ class ParameterKernel(torch.nn.Module):
         return K_elm
     
     def get_kernel_dict(self, Z, desc, expK=1):
-        Zflat = list(chain(*[z_i.tolist() for z_i in Z]))
-        elements = sorted(set(Zflat))
+        nondummy = (Z > 0).reshape(-1)
+        elements = sorted(set(Z.reshape(-1)[nondummy].tolist()))
         K_dict = {}
         for elm in elements:
             ## get atomistic neighborhood descriptors for element
@@ -51,9 +66,9 @@ class ParameterKernel(torch.nn.Module):
         return K
         
     def forward(self, Alpha, Z, desc, expK=1):
-        Zflat = list(chain(*[z_i.tolist() for z_i in Z]))
-        elements = sorted(set(Zflat))
-        Zall = torch.tensor(Zflat)
+        nondummy = (Z > 0).reshape(-1)
+        Zall = Z.reshape(-1)[nondummy]
+        elements = sorted(set(Zall.tolist()))
         if any(elm not in self.elements_ref for elm in elements):
             raise ValueError("Elements in reference and call inconsistent!")
         K = self.get_kernel_dict(Z, desc, expK=expK)
@@ -63,31 +78,22 @@ class ParameterKernel(torch.nn.Module):
                            elements))
         elm_idx = [torch.where(Zall==elm)[0] for elm in elements]
         for i, idx in enumerate(elm_idx): y[:,idx] = Alpha_K[i]
-#        for elm in elements:
-#            ## get atomistic neighborhood descriptors for element
-#            idx_in   = [torch.where(s==elm)[0] for s in Z]
-#            K_elm    = self.get_element_kernel(elm, idx_in, desc, expK=expK)
-#            y_elm    = torch.matmul(Alpha[:,self.idx_ref[elm]], K_elm)
-#            ## map to total prediction
-#            idx      = torch.where(Zall==elm)[0]
-#            y[:,idx] = y_elm
         return y
         
     
-class AMASEQC_singlepoint(torch.nn.Module):
+class AMASE_singlepoint_core(torch.nn.Module):
     #TODO: Allow for `reference_desc` to be callable
     #TODO: Typing, some refactoring, clean-up, docs
     def __init__(self, reference_Z, reference_desc, reference_coordinates=None,
                  seqm_settings={}, parameters=None, mode="full", 
                  custom_reference=False):
-        super(AMASEQC_singlepoint, self).__init__()
+        super(AMASE_singlepoint_core, self).__init__()
         self.param_dir = seqm_settings.get("parameter_file_dir", "nodirdefined")
         self.method = seqm_settings.get("method", "nomethoddefined")
         if self.method == "nomethoddefined":
             raise ValueError("`seqm_settings` has to include 'method'")
         
-        Zflat = list(chain(*[z_i.tolist() for z_i in reference_Z]))
-        self.n_ref = len(Zflat)
+        self.n_ref = torch.count_nonzero(reference_Z)
         if callable(reference_desc): raise NotImplementedError
         self.kernel = ParameterKernel(reference_Z, reference_desc)
         if parameters is None:
@@ -103,8 +109,9 @@ class AMASEQC_singlepoint(torch.nn.Module):
                 self.process_prediction = self.delta_default
         else:
             raise ValueError("Unknown mode '"+mode+"'.")
-        self.runner = SEQM_singlepoint(seqm_settings)
-        
+        self.seqm_runner = SEQM_singlepoint_core(seqm_settings)
+        self.results = {}
+    
     def __eq__(self, other):
         if self.__class__ != other.__class__: return False
         return self.__dict__ == other.__dict__
@@ -121,28 +128,69 @@ class AMASEQC_singlepoint(torch.nn.Module):
     def custom_delta(self, par, reference_par=None, **kwargs):
         return reference_par + par
     
-    def forward(self, Alpha, Z, positions, desc, reference_params=None):
+    def forward(self, Alpha, Z, positions, desc, reference_params=None, expK=1):
         msg = "`Alpha` inconsistent with (`parameters`, number of references)"
         assert (Alpha.shape == (len(self.parameters),self.n_ref)), msg
-        species = prepare_array(Z, "atomic numbers")
-        coordinates = prepare_array(positions, "coordinates")
         if callable(desc): raise NotImplementedError
-        pred = self.kernel(Alpha, Z, desc)
-        p = self.process_prediction(pred, species=species, 
+        pred = self.kernel(Alpha, Z, desc, expK=expK)
+        p = self.process_prediction(pred, species=Z, 
                                     reference_par=reference_params)
-        result = self.runner(p, species, coordinates, 
-                             custom_params=self.parameters)
-        return result
+        res = self.seqm_runner(p, Z, positions, custom_params=self.parameters)
+        self.results = self.seqm_runner.results
+        return res
+    
+    def get_property(self, property_name):
+        if not property_name in self.results:
+            raise ValueError("Property '"+property_name+"' not available.")
+        return self.results[property_name]
         
     
-class AMASEQC_multirun(torch.nn.Module):
+
+class AMASE_singlepoint(torch.nn.Module):
+    def __init__(self, reference_Z, reference_desc, reference_coordinates=None,
+                 seqm_settings={}, parameters=None, mode="full",
+                 custom_reference=False):
+        super(AMASE_singlepoint, self).__init__()
+        Z_ref = prepare_array(reference_Z, "atomic numbers")
+        self.core_runner = AMASE_singlepoint_core(Z_ref, reference_desc, 
+                reference_coordinates=reference_coordinates, 
+                seqm_settings=seqm_settings, parameters=parameters, mode=mode,
+                custom_reference=custom_reference)
+    
+    def __eq__(self, other):
+        if self.__class__ != other.__class__: return False
+        return self.__dict__ == other.__dict__
+    
+    def forward(self, Alpha, Z, positions, desc, reference_params=None, expK=1):
+        orderer = Orderator(Z, positions)
+        species, xyz = orderer.prepare_input()
+        xyz.requires_grad_(True)
+        res = self.core_runner(Alpha, species, xyz, desc, expK=expK,
+                               reference_params=reference_params)
+        self.results = self.core_runner.results
+        return res
+    
+    def get_property(self, property_name):
+        if not property_name in self.results:
+            raise ValueError("Property '"+property_name+"' not available.")
+        return self.results[property_name]
+        
+    
+
+class AMASE_multirun_core(torch.nn.Module):
     #TODO: Allow for `reference_desc` to be callable
     #TODO: Typing, refactor, clean-up, docs
     def __init__(self, Z, desc, coordinates, reference_Z, reference_desc, 
                  reference_coordinates=None, seqm_settings={}, mode="full", 
                  parameters=None, custom_reference=None, expK=1):
-        super(AMASEQC_multirun, self).__init__()
+        super(AMASE_multirun_core, self).__init__()
         # check input
+        if not torch.is_tensor(Z):
+            msg = "In core routine, atomic numbers have to be a tensor!"
+            raise ValueError(msg)
+        if not torch.is_tensor(coordinates):
+            msg = "In core routine, coordinates have to be a tensor!"
+            raise ValueError(msg)
         self.param_dir = seqm_settings.get("parameter_file_dir", "nodirdefined")
         if self.param_dir == "nodirdefined":
             raise ValueError("`seqm_settings` has to include 'parameter_file_dir'")
@@ -158,10 +206,8 @@ class AMASEQC_multirun(torch.nn.Module):
             self.parameters = parameters
         
         # default parameters for method (as reference or as template)
-        species = prepare_array(Z, "atomic numbers")
-        p_def = get_default_parameters(species,
-                            method=self.method, parameter_dir=self.param_dir,
-                            param_list=self.parameters)
+        p_def = get_default_parameters(Z, method=self.method, 
+                    parameter_dir=self.param_dir, param_list=self.parameters)
         # set p0 depending on mode (final p = prediction + p0)
         if mode == "full":
             self.p0 = torch.zeros_like(p_def)
@@ -175,28 +221,28 @@ class AMASEQC_multirun(torch.nn.Module):
         self.p0.requires_grad_(False)
         
         # elements and indexing for input structures
-        Zflat = list(chain(*[z_i.tolist() for z_i in Z]))
-        elements = sorted(set(Zflat))
-        Zall = torch.tensor(Zflat)
+        nondummy = (Z > 0).reshape(-1)
+        Zall = Z.reshape(-1)[nondummy]
+        elements = sorted(set(Zall.tolist()))
         self.elm_idx = [torch.where(Zall==elm)[0] for elm in elements]
         # elements and indexing for reference structures
-        Zflat = list(chain(*[z_i.tolist() for z_i in reference_Z]))
-        ref_elements = sorted(set(Zflat))
+        nondummy = (reference_Z > 0).reshape(-1)
+        Zall = reference_Z.reshape(-1)[nondummy]
+        ref_elements = sorted(set(Zall.tolist()))
         if any(elm not in ref_elements for elm in elements):
             msg  = "Some element(s) of requrested systems not in reference "
             msg += "structures! Aborting."
             raise ValueError(msg)
-        Zall = torch.tensor(Zflat)
         self.ref_idx = [torch.where(Zall==elm)[0] for elm in ref_elements]
         
         # build kernel matrix
         kernel = ParameterKernel(reference_Z, reference_desc)
         self.K = kernel.get_sorted_kernel(elements, Z, desc, expK=expK)
         # set up multirun calculator
-        positions = prepare_array(coordinates, "coordinates")
-        self.runner = SEQM_multirun(species, positions,
-                                    custom_params=self.parameters,
-                                    seqm_settings=seqm_settings)
+        self.seqm_runner = SEQM_multirun_core(Z, coordinates,
+                                              custom_params=self.parameters,
+                                              seqm_settings=seqm_settings)
+        self.results = {}
     
     def __eq__(self, other):
         if self.__class__ != other.__class__: return False
@@ -208,8 +254,43 @@ class AMASEQC_multirun(torch.nn.Module):
                            self.ref_idx, self.K))
         for i, idx in enumerate(self.elm_idx): pred[:,idx] = Alpha_K[i]
         p = pred + self.p0
-        result = self.runner(p)
-        return result
+        res = self.seqm_runner(p)
+        self.results = self.seqm_runner.results
+        return res
+    
+    def get_property(self, property_name):
+        if not property_name in self.results:
+            raise ValueError("Property '"+property_name+"' not available.")
+        return self.results[property_name]
+        
+    
+class AMASE_multirun(torch.nn.Module):
+    def __init__(self, Z, desc, coordinates, reference_Z, reference_desc,
+                 reference_coordinates=None, seqm_settings={}, mode="full",
+                 parameters=None, custom_reference=None, expK=1):
+        super(AMASE_multirun, self).__init__()
+        Z_ref = prepare_array(reference_Z, "atomic numbers")
+        orderer = Orderator(Z, coordinates)
+        species, xyz = orderer.prepare_input()
+        xyz.requires_grad_(True)
+        self.core_runner = AMASE_multirun_core(species, desc, xyz, Z_ref, 
+                reference_desc, reference_coordinates=reference_coordinates,
+                seqm_settings=seqm_settings, mode=mode, expK=expK,
+                parameters=parameters, custom_reference=custom_reference)
+    
+    def __eq__(self, other):
+        if self.__class__ != other.__class__: return False
+        return self.__dict__ == other.__dict__
+    
+    def forward(self, Alpha):
+        res = self.core_runner(Alpha)
+        self.results = self.core_runner.results
+        return res
+    
+    def get_property(self, property_name):
+        if not property_name in self.results:
+            raise ValueError("Property '"+property_name+"' not available.")
+        return self.results[property_name]
         
     
 
--- seqm/utils/seqm_runners.py
+++ seqm/utils/seqm_runners.py
@@ -1,8 +1,21 @@
+############################################################################
+# Utilities for running SEQM calculations                                  #
+#  - SEQM_singlepoint: parameters, species, coordinates defined at runtime #
+#  - SEQM_multirun: parameters at runtime, systems fixed (for training)    #
+#                                                                          #
+# Curent (Feb/06): Basic implementation                                    #
+# TODO:  . typing                                                          #
+#        . refactor                                                        #
+#        . ?add MD engine?                                                 #
+#        . custom backwards for (unlikely) RuntimeErrors in foward         #
+############################################################################
+
 import torch
+from itertools import chain
 from torch.autograd import grad as agrad
 from seqm.basics import Parser, Energy
 from seqm.seqm_functions.constants import Constants
-from .pyseqm_helpers import prepare_array, Orderator
+from .pyseqm_helpers import Orderator
 
 
 LFAIL = torch.tensor(torch.inf)
@@ -17,18 +30,21 @@ else:
     sp2_def = [False]
 
 
-class SEQM_singlepoint(torch.nn.Module):
+default_settings = {
+                    'method'            : 'AM1',
+                    'scf_eps'           : 1.0e-6,
+                    'scf_converger'     : [0,0.15],
+                    'scf_backward'      : 2,
+                    'sp2'               : sp2_def,
+                    'pair_outer_cutoff' : 1.0e10,
+                    'Hf_flag'           : False,
+                   }
+
+
+class SEQM_singlepoint_core(torch.nn.Module):
     def __init__(self, seqm_settings=None):
-        super(SEQM_singlepoint, self).__init__()
-        self.settings = {
-                         'method'            : 'AM1',
-                         'scf_eps'           : 1.0e-6,
-                         'scf_converger'     : [0,0.15],
-                         'scf_backward'      : 2,
-                         'sp2'               : sp2_def,
-                         'pair_outer_cutoff' : 1.0e10,
-                         'Hf_flag'           : False,
-                        }
+        super(SEQM_singlepoint_core, self).__init__()
+        self.settings = default_settings
         self.settings.update(seqm_settings)
         self.const = Constants()
         self.results = {}
@@ -40,23 +56,21 @@ class SEQM_singlepoint(torch.nn.Module):
 #    @staticmethod
     def forward(self, p, species, coordinates, custom_params=[]):
 #    TODO: NEED CUSTOM BACKWARD FOR WHEN CALCULTION FAILS (RETURN NaN).
-#          IS p.register_backward_hook(lambda grad: grad * NaN) working?
+#          IS p.register_hook(lambda grad: grad * NaN) working?
 #    def forward(self, ctx, p):
         """ Run calculation. """
-        self.orderer = Orderator(species, coordinates)
-        Z, xyz = self.orderer.prepare_input()
-        xyz.requires_grad_(True)
-        elements = sorted(set([0] + Z.reshape(-1).tolist()))
+        elements = sorted(set([0] + species.reshape(-1).tolist()))
         learnedpar = {par:p[i] for i, par in enumerate(custom_params)}
         self.settings['elements'] = torch.tensor(elements)
         self.settings['learned'] = custom_params
         self.settings['eig'] = True
         calc = Energy(self.settings)
         try:
-            res = calc(self.const, xyz, Z, learnedpar, all_terms=True)
+            res = calc(self.const, coordinates, species, learnedpar, 
+                       all_terms=True)
         except RuntimeError:
-            p.register_backward_hook(lambda grad: grad * torch.nan)
-            xyz.register_backward_hook(lambda grad: grad * torch.nan)
+            p.register_hook(lambda grad: grad * torch.nan)
+            coordinates.register_hook(lambda grad: grad * torch.nan)
             return {'atomization':torch.nan, 'energy':torch.nan, 
                     'forces':torch.nan*torch.ones_like(xyz), 'gap':torch.nan}
         masking = (~res[-1]).float()
@@ -64,12 +78,11 @@ class SEQM_singlepoint(torch.nn.Module):
         Eat_fin = res[0] * masking
         Etot_fin = res[1] * masking
         # forces
-        F_raw = -agrad(res[1].sum(), xyz, create_graph=True)[0]
-        F_reordered = self.orderer.reorder(F_raw)
-        F_fin = F_reordered * masking[...,None,None]
+        F = -agrad(res[1].sum(), coordinates, create_graph=True)[0]
+        F_fin = F * masking[...,None,None]
         # HOMO-LUMO gap
         my_parser = Parser(calc.seqm_parameters)
-        n_occ = my_parser(self.const, Z, xyz)[4]
+        n_occ = my_parser(self.const, species, coordinates)[4]
         homo = (n_occ-1).unsqueeze(-1)
         lumo = n_occ.unsqueeze(-1)
         ehomo = torch.gather(res[6], 1, homo).reshape(-1)
@@ -88,23 +101,52 @@ class SEQM_singlepoint(torch.nn.Module):
         
     
 
-class SEQM_multirun(torch.nn.Module):
+class SEQM_singlepoint(torch.nn.Module):
+    def __init__(self, seqm_settings=None):
+        super(SEQM_singlepoint, self).__init__()
+        self.settings = seqm_settings
+        self.core_runner = SEQM_singlepoint_core(seqm_settings)
+    
+    def __eq__(self, other):
+        if self.__class__ != other.__class__: return False
+        return self.__dict__ == other.__dict__
+    
+    def forward(self, p, species, coordinates, custom_params=[]):
+        self.orderer = Orderator(species, coordinates)
+        Z_padded = self.orderer.species
+        Z, xyz = self.orderer.prepare_input()
+        xyz.requires_grad_(True)
+        nodummy = (Z_padded > 0)
+        Z_plain = [s[nodummy[i]] for i, s in enumerate(Z_padded)]
+        nAtoms = torch.count_nonzero(Z, dim=1)
+        shifts = [0]+torch.cumsum(nAtoms, 0).tolist()[:-1]
+        subsort = [torch.argsort(s, descending=True) for s in Z_plain]
+        ragged_idx = [(s+shifts[i]).tolist() for i, s in enumerate(subsort)]
+        p_sorting = torch.tensor(list(chain(*ragged_idx)))
+        p_sorted = p[:,p_sorting]
+        res = self.core_runner(p_sorted, Z, xyz, custom_params=custom_params)
+        #TODO: DOUBLE-CHECK IF THIS IS NEEDED!
+#        F_resort = self.orderer.reorder(F)
+#        res[2] = F_resort
+        self.results = self.core_runner.results
+#        self.results['forces'] = F_resort
+        return res
+    
+    def get_property(self, property_name):
+        if not property_name in self.results:
+            raise ValueError("Property '"+property_name+"' not available.")
+        return self.results[property_name]
+        
+    
+
+class SEQM_multirun_core(torch.nn.Module):
     def __init__(self, species, coordinates, custom_params=[], 
                  seqm_settings=None):
-        super(SEQM_multirun, self).__init__()
-        settings = {
-                    'method'            : 'AM1',
-                    'scf_eps'           : 1.0e-6,
-                    'scf_converger'     : [0,0.15],
-                    'scf_backward'      : 2,
-                    'sp2'               : sp2_def,
-                    'pair_outer_cutoff' : 1.0e10,
-                    'Hf_flag'           : False,
-                   }
+        super(SEQM_multirun_core, self).__init__()
+        settings = default_settings
         settings.update(seqm_settings)
         self.const = Constants()
-        self.orderer = Orderator(species, coordinates)
-        self.Z, self.xyz = self.orderer.prepare_input()
+        self.Z, self.xyz = species, coordinates
         self.xyz.requires_grad_(True)
         self.custom_par = custom_params
         elements = sorted(set([0] + self.Z.reshape(-1).tolist()))
@@ -125,7 +167,7 @@ class SEQM_multirun(torch.nn.Module):
 #    @staticmethod
     def forward(self, p):
 #    TODO: NEED CUSTOM BACKWARD FOR WHEN CALCULTION FAILS (RETURN NaN).
-#          IS p.register_backward_hook(lambda grad: grad * NaN) working?
+#          IS p.register_hook(lambda grad: grad * NaN) working?
 #    def forward(self, ctx, p):
         """ Run calculation. """
         learnedpar = {par:p[i] for i, par in enumerate(self.custom_par)}
@@ -133,15 +175,14 @@ class SEQM_multirun(torch.nn.Module):
             res = self.calc(self.const, self.xyz, self.Z, learnedpar,
                             all_terms=True)
         except RuntimeError:
-            p.register_backward_hook(lambda grad: grad * torch.nan)
-            self.xyz.register_backward_hook(lambda grad: grad * torch.nan)
+            p.register_hook(lambda grad: grad * torch.nan)
+            self.xyz.register_hook(lambda grad: grad * torch.nan)
             return torch.nan, torch.nan, torch.nan*torch.ones_like(self.xyz), torch.nan
         masking = (~res[-1]).float()
         Eat_fin = res[0] * masking
         Etot_fin = res[1] * masking
-        F_raw = -agrad(res[1].sum(), self.xyz, create_graph=True)[0]
-        F_reordered = self.orderer.reorder(F_raw)
-        F_fin = F_reordered * masking[...,None,None]
+        F = -agrad(res[1].sum(), self.xyz, create_graph=True)[0]
+        F_fin = F * masking[...,None,None]
         ehomo = torch.gather(res[6], 1, self.homo).reshape(-1)
         elumo = torch.gather(res[6], 1, self.lumo).reshape(-1)
         gap_fin = (elumo - ehomo) * masking
@@ -150,4 +191,49 @@ class SEQM_multirun(torch.nn.Module):
         self.results['forces'] = F_fin
         self.results['gap'] = gap_fin
         return Eat_fin, Etot_fin, F_fin, gap_fin
+        
+    def get_property(self, property_name):
+        if not property_name in self.results:
+            raise ValueError("Property '"+property_name+"' not available.")
+        return self.results[property_name]
+        
+    
+class SEQM_multirun(torch.nn.Module):
+    def __init__(self, species, coordinates, custom_params=[],
+                 seqm_settings=None):
+        super(SEQM_multirun, self).__init__()
+        self.orderer = Orderator(species, coordinates)
+        Z_padded = self.orderer.species
+        Z, xyz = self.orderer.prepare_input()
+        xyz.requires_grad_(True)
+        nodummy = (Z_padded > 0)
+        Z_plain = [s[nodummy[i]] for i, s in enumerate(Z_padded)]
+        nAtoms = torch.count_nonzero(Z, dim=1)
+        shifts = [0]+torch.cumsum(nAtoms, 0).tolist()[:-1]
+        subsort = [torch.argsort(s, descending=True) for s in Z_plain]
+        ragged_idx = [(s+shifts[i]).tolist() for i, s in enumerate(subsort)]
+        self.p_sorting = torch.tensor(list(chain(*ragged_idx)))
+        self.core_runner = SEQM_multirun_core(Z, xyz, 
+                custom_params=custom_params, seqm_settings=seqm_settings)
+    
+    def __eq__(self, other):
+        if self.__class__ != other.__class__: return False
+        return self.__dict__ == other.__dict__
+    
+    def forward(self, p):
+        p_sorted = p[:,self.p_sorting]
+        res = self.core_runner(p_sorted)
+        #TODO: DOUBLE-CHECK IF THIS IS NEEDED!
+#        F_resort = self.orderer.reorder(F)
+#        res[2] = F_resort
+        self.results = self.core_runner.results
+#        self.results['forces'] = F_resort
+        return res
+    
+    def get_property(self, property_name):
+        if not property_name in self.results:
+            raise ValueError("Property '"+property_name+"' not available.")
+        return self.results[property_name]
+        
+    
 

implicit autodiff implementation (fixes gradients)

seqm/Molecule.py
@@ -4,7 +4,7 @@ import time
 from .basics import Parser
 
 class Molecule(torch.nn.Module):
-    def __init__(self, const, seqm_parameters, coordinates, species, charge=0, mult=1, *args, **kwargs):
+    def __init__(self, const, seqm_parameters, coordinates, species, charges=0, mult=1, *args, **kwargs):
         """
         unit for timestep is femtosecond
         output: [molecule id list, frequency N, prefix]
@@ -17,9 +17,9 @@ class Molecule(torch.nn.Module):
         self.seqm_parameters = seqm_parameters
         self.coordinates = coordinates
         self.species = species
-        if not torch.is_tensor(charge):
-            charge = charge * torch.ones(coordinates.size()[0], device=coordinates.device)
-        self.tot_charge = charge
+        if not torch.is_tensor(charges):
+            charges = charges * torch.ones(coordinates.size()[0], device=coordinates.device)
+        self.tot_charge = charges
         if not torch.is_tensor(mult):
             mult = mult * torch.ones(coordinates.size()[0], device=coordinates.device)
         self.mult = mult
@@ -53,7 +53,7 @@ class Molecule(torch.nn.Module):
         self.e_mo = None #
         self.e_gap = None #
         
-        self.charge = None
+        self.charges = None
         self.dipole = None
         
         self.Electronic_entropy = None

seqm/utils/seqm_core_runners.py
@@ -12,6 +12,7 @@
 import torch
 from torch.autograd import grad as agrad
 from ..basics import Parser, Energy
+from ..Molecule import Molecule
 from ..seqm_functions.constants import Constants
 from .pyseqm_helpers import get_default_parameters
 from ..seqm_functions.parameters import params
@@ -34,7 +35,7 @@ default_settings = {
                     'method'            : 'AM1',
                     'scf_eps'           : 1.0e-6,
                     'scf_converger'     : [0,0.15],
-                    'scf_backward'      : 2,
+                    'scf_backward'      : 1,
                     'scf_backward_eps'  : 1e-4,
                     'sp2'               : sp2_def,
                     'pair_outer_cutoff' : 1.0e10,
@@ -150,10 +151,10 @@ class SEQM_singlepoint_core(torch.nn.Module):
         self.settings['learned'] = self.custom_params
         self.settings['eig'] = True
         coordinates.requires_grad_(True)
+        mol = Molecule(self.const, self.settings, coordinates, species)
         calc = Energy(self.settings).to(device)
 #        try:
-        res = calc(self.const, coordinates, species, learnedpar, 
-                       all_terms=True)
+        res = calc(mol, learnedpar, all_terms=True)
 #        except:# (RuntimeError, torch._C._LinAlgError):
 #            p.register_hook(lambda grad: grad * 0.)
 #            coordinates.register_hook(lambda grad: grad * 0.)
@@ -171,13 +172,14 @@ class SEQM_singlepoint_core(torch.nn.Module):
         F_fin = F * masking[...,None,None]
         coordinates.requires_grad_(False)
         # HOMO-LUMO gap
-        my_parser = Parser(calc.seqm_parameters)
-        n_occ = my_parser(self.const, species, coordinates)[4]
-        homo = (n_occ-1).unsqueeze(-1)
-        lumo = n_occ.unsqueeze(-1)
-        ehomo = torch.gather(res[6], 1, homo).reshape(-1)
-        elumo = torch.gather(res[6], 1, lumo).reshape(-1)
-        gap_fin = (elumo - ehomo) * masking
+        gap_fin = res[6] * masking
+#        my_parser = Parser(calc.seqm_parameters)
+#        n_occ = my_parser(self.const, species, coordinates)[4]
+#        homo = (n_occ-1).unsqueeze(-1)
+#        lumo = n_occ.unsqueeze(-1)
+#        ehomo = torch.gather(res[6], 1, homo).reshape(-1)
+#        elumo = torch.gather(res[6], 1, lumo).reshape(-1)
+#        gap_fin = (elumo - ehomo) * masking
         # update self.results dict
         self.results['atomization'] = Eat_fin
         self.results['energy'] = Etot_fin

tests/derivative_tests.py
@@ -8,16 +8,20 @@ seqm.seqm_functions.scf_loop.debug = False
 seqm.seqm_functions.diag.DEGEN_EIGENSOLVER = True
 seqm.seqm_functions.scf_loop.SCF_BACKWARD_MAX_ITER = 400
 seqm.seqm_functions.scf_loop.SCF_IMPLICIT_BACKWARD = True
-seqm.seqm_functions.scf_loop.SCF_BACKWARD_ANDERSON_TOLERANCE = 1e-6
-seqm.seqm_functions.scf_loop.SCF_BACKWARD_ANDERSON_MAXITER = 200
-seqm.seqm_functions.scf_loop.SCF_BACKWARD_ANDERSON_HISTSIZE = 12
+seqm.seqm_functions.scf_loop.SCF_BACKWARD_ANDERSON_TOLERANCE = 1e-4
+seqm.seqm_functions.scf_loop.SCF_BACKWARD_ANDERSON_MAXITER = 40
+seqm.seqm_functions.scf_loop.SCF_BACKWARD_ANDERSON_HISTSIZE = 5
+
+seqm.seqm_functions.scf_loop.SCF_BACKWARD_MAX_ITER = 200
+
+
 torch.set_default_dtype(torch.double) # safe choice for finite differences in gradcheck
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 
 prop = "energy"#"gap"
-variable = "param"#"coords"#"param"
+variable = "coords"#"param"
 uhf = True
 
 if uhf:
@@ -59,7 +63,7 @@ seqm_parameters = {
                    'learned'           : lpar,
                    'pair_outer_cutoff' : 1e10, 
                    'scf_backward'      : 0,
-                   'scf_backward_eps'  : 1e-12,
+                   'scf_backward_eps'  : 1e-9,
                    'parameter_file_dir': '/home/martin/work/software/PYSEQM/seqm/params/',
                    'eig'               : prop=="gap",
                    'UHF'               : uhf,

tests/test15/plot.py
@@ -2,10 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 try:
-    fn=sys.argv[1]
+    fn0=sys.argv[1]
+    fn1=sys.argv[2]
+    fn2=sys.argv[3]
 except:
-    fn='log.dat'
-d=np.loadtxt(fn)
+    fn0='log0.dat'
+    fn1='log1.dat'
+    fn2='log2.dat'
+
+d=np.loadtxt(fn0)
+d1=np.loadtxt(fn1)
+d2=np.loadtxt(fn2)
 ##index, dx, fy, dfydx
 
 t=np.argmin(d[:,2])
@@ -20,30 +27,62 @@ print(d[t,1])
 dx = d[1,1]-d[0,1]
 
 x1 = d[1:-1,1]
+# FD force
+f0 = -(d[2:,4]-d[:-2,4])/dx/2.0
+# FD Hessian
 f1 = (d[2:,2]-d[:-2,2])/dx/2.0
 plt.figure(0)
-plt.plot(d[:,1],d[:,3],'r',label='from code')
-plt.plot(x1,f1,'g--', label='numerical diff')
-plt.plot(d[:,1], np.zeros_like(d[:,1]))
+plt.plot(d[:,1],d[:,3],'r',label='no SCFbackward')
+plt.plot(d1[:,1],d1[:,3],'b',label='recursive formula')
+plt.plot(d2[:,1],d2[:,3],'g',label='direct backprop')
+plt.plot(x1, f1, 'k--', label='numerical diff')
+plt.plot(d[:,1], np.zeros_like(d[:,1]), c='0.5', ls=':')
 plt.legend()
 plt.ylabel('dfz_dz = d^2E/dz^2 (eV/Angstrom^2)')
 plt.xlabel('z (Angstrom)')
 #plt.xlim([0.0,10])
 
 plt.figure(1)
-df = d[1:-1,3]-f1
-plt.plot(x1[:],df[:])
-plt.ylabel('dfz_dz error (eV/Angstrom^2)')
+e = d[1:-1,3]-f1
+e1 = d1[1:-1,3]-f1
+e2 = d2[1:-1,3]-f1
+plt.plot(x1, e, 'r',label='no SCFbackward')
+plt.plot(x1, e1, 'b',label='recursive formula')
+plt.plot(x1, e2, 'g',label='direct backprop')
+plt.ylabel('|dfz_dz error| (eV/Angstrom^2)')
 plt.xlabel('z (Angstrom)')
+plt.legend()
+#plt.xlim([0.0,10])
+
+plt.figure(2)
+plt.plot(x1, e/f1, 'r',label='no SCFbackward')
+plt.plot(x1, e1/f1, 'b',label='recursive formula')
+plt.plot(x1, e2/f1, 'g',label='direct backprop')
+plt.ylabel('dfz_dz rel. error (eV/Angstrom^2)')
+plt.xlabel('z (Angstrom)')
+plt.legend()
 #plt.xlim([0.0,10])
 
 
-f0 = -(d[2:,4]-d[:-2,4])/dx/2.0
-plt.figure(2)
-plt.plot(d[:,1], d[:,2])
-plt.plot(d[1:-1,1], f0, 'g--')
+plt.figure(3)
+plt.plot(d[:,1], d[:,2], 'r')
+plt.plot(d1[:,1], d1[:,2], 'b')
+plt.plot(d2[:,1], d2[:,2], 'g')
+plt.plot(d[1:-1,1], f0, 'k--')
 plt.ylabel('fz (eV/Angstrom)')
 plt.xlabel('z (Angstrom)')
+#plt.xlim([0.0,10])
+
+plt.figure(4)
+e = d[1:-1,2]-f0
+e1 = d1[1:-1,2]-f0
+e2 = d2[1:-1,2]-f0
+plt.plot(x1, e, 'r',label='no SCFbackward')
+plt.plot(x1, e1, 'b:',label='recursive formula')
+plt.plot(x1, e2, 'g--',label='direct backprop')
+plt.ylabel('|fz error| (eV/Angstrom)')
+plt.xlabel('z (Angstrom)')
+plt.legend()
 #plt.xlim([0.0,10])
 
 plt.show()
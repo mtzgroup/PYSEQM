5b820a31b09a2fc7b16fdf06476bc272edb3b0a9
Martin Stoehr <martin.stoehr.research@gmail.com>
2024-11-05 13:59:26 -0800

    scf_loop: correct MOs for single mol calc

--- seqm/seqm_functions/scf_loop.py
+++ seqm/seqm_functions/scf_loop.py
@@ -961,6 +961,8 @@ def scf_loop(const, molsize, nHeavy, nHydro, nOccMO, \
         charge = None
         if type(v) is tuple:
             v = tuple(v_i.transpose(-1,-2) for v_i in v)
+        elif torch.is_tensor(v):
+            v = v.transpose(-1,-2)
         return F, e, v, Pconv, Hcore, w, charge, notconverged
     else:
         return F, None, None, Pconv, Hcore, w, None, notconverged

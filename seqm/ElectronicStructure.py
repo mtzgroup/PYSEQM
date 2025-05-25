496b03fc44af03aeb6c4b56792952bda394f030a
Martin Stoehr <martin.stoehr.research@gmail.com>
2025-05-25 10:35:45 -0700

    ES: switch to nested tensors in HA

--- seqm/ElectronicStructure.py
+++ seqm/ElectronicStructure.py
@@ -1,5 +1,6 @@
 import torch
 from torch.autograd import grad as adgrad
+from torch.nested import as_nested_tensor
 from .basics import *
 from seqm.XLBOMD import ForceXL
 #from seqm.XLBOMD_LR import ForceXL as ForceXL_lr
@@ -7,11 +8,12 @@ from seqm.XLBOMD import ForceXL
 
 
 def eckart(masses, coordinates):
-    COMs = (masses / masses.sum(dim=1).unsqueeze(-1)).unsqueeze(-1) * coordinates
-    R = coordinates - COMs
-    I = torch.einsum("bij,bik->bjk", masses.unsqueeze(-1) * R, R)
-    _, X = torch.linalg.eigh(I / masses.sum(dim=1)[...,None,None])
-    R_Eckart = R.bmm(X)
+    M_mol = masses.sum(dim=(1,2))
+    COMs = (masses * coordinates).sum(dim=1) / M_mol
+    R0 = coordinates - COMs
+    I = torch.einsum("bij,bik->bjk", masses * R0, R0)
+    _, X = torch.linalg.eigh(I / M_mol)
+    R_Eckart = R0.bmm(X)
     return X, R_Eckart
 
 def vibrational_basis(masses, coordinates):
@@ -19,28 +21,49 @@ def vibrational_basis(masses, coordinates):
     X, R_Eck = eckart(masses, coordinates)
     # Eigenbasis for translations and rotations
     T = torch.eye(3).repeat(*coordinates.shape[:2], 1)
-    G_Eck = R_Eck * masses.sqrt().unsqueeze(-1)
+    G_Eck = R_Eck * masses.sqrt()
     R = torch.stack([torch.cat([torch.stack([gij.cross(xi[i]) for i in range(3)])
                      for gij in gi]) for (gi, xi) in zip(G_Eck, X)])
     TR = torch.cat((T, R), 2)
-    # Null space of global TR = net translations and rotations
-    # -> 6 (5) zero singular values for non-linear (linear) molecules
+    # Eigenspace of global TR = net translations and rotations
+    # -> remove subspace corresponding to the 6 (5) non-zero
+    #    singular values for non-linear (linear) molecules
     U, S, _ = torch.linalg.svd(TR, full_matrices=True)
-    # mask null space of global TR = projected vibrational basis
-    null_mask = torch.where(S.unsqueeze(1).repeat(1,U.shape[1],1) > 1e-15)
-    U[null_mask] = 0.
-    return U
+    nn0 = S.round_(decimals=15).count_nonzero(dim=-1)
+    B = as_nested_tensor([Ui[:,n:] for (Ui,n) in zip(U, nn0)])
+    return B
+
+def tuple2nested(x): return as_nested_tensor(list(x))
 
 def harmonic_analysis(masses, coordinates, hessian):
-    m = masses.pow(-0.5).repeat_interleave(3, dim=-1)
-    Hm = torch.einsum('bi,bj->bij', (m, m)) * hessian
+    im = masses.pow(-0.5).repeat_interleave(3, dim=1).squeeze(-1)
+#    Hm = torch.einsum('bi,bj->bij', (m, m)) * hessian
+    Hm = as_nested_tensor([im_i.unsqueeze(-1) * Hi * im_i for (im_i,Hi) in zip(im, hessian)])
     B = vibrational_basis(masses, coordinates)
     BTHB = B.transpose(-1,-2).bmm(Hm.bmm(B))
-    e, v = torch.linalg.eigh(BTHB)
+#    e, v = torch.linalg.eigh(BTHB)
+    # update once `eigh` supported by nested
+    e, v = map(tuple2nested, zip(*map(torch.linalg.eigh, BTHB)))
     U = B.bmm(v)
-    Q = U * torch.einsum('bi,bj->bij', (m, torch.ones_like(U[:,0,:])))
+    M = (im_i.unsqueeze(-1) for im_i in im)
+    Q = nestedmap_binary(torch.mul, U, M)
+#    Q = U * torch.einsum('bi,bj->bij', (im, torch.ones_like(U[:,0,:])))
     return e, Q.transpose(-1,-2)
 
+def nestedmap_unary(f, x):
+    ## update once complex supported in `as_nested_tensor`
+    return torch.nested.nested_tensor(list(map(f, x)))
+
+def nestedmap_binary(f, x, y):
+    ## update once complex supported in `as_nested_tensor`
+    return torch.nested.nested_tensor(list(map(f, x, y)))
+
+def curve2freq(x):
+    # update once `type` and `sqrt` supported by nested
+    freq = x.type(torch.complex64).sqrt()
+    ## ignore imaginary freqencies small than 1 cm^-1
+    if (freq.imag.abs() < 1.9176525463471034e-3).all(): freq = freq.real
+    return freq * 0.06465415105180661 ## curvature to frequency
 
 
 class Electronic_Structure(torch.nn.Module):
@@ -100,12 +123,9 @@ class Electronic_Structure(torch.nn.Module):
                                               ).view(molecule.nmol, _3n, _3n)
                 
                 if not self.keep_graph: molecule.force.detach_()
-                freq2, molecule.vib_modes = harmonic_analysis(molecule.mass.squeeze(-1), molecule.coordinates,
+                curve, molecule.vib_modes = harmonic_analysis(molecule.mass, molecule.coordinates,
                                                               molecule.hessian)
-                freq = freq2.type(torch.complex64).sqrt()
-                ## ignore negative eigenvalues corresponding to less than 1 cm-1
-                if (freq.imag.abs() < 1.9176525463471034e-3).all(): freq = freq.real
-                molecule.vib_freqs = freq * 0.06465415105180661 ## curvature to frequency
+                molecule.vib_freqs = nestedmap_unary(curve2freq, curve)
             molecule.dm = P.detach()
             molecule.mo_coeff = C#.detach() # detach needed?
             
